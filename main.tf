770590f6c6f6242eebb0b3931c58dfedd7b6b54b
you <you@example.com>
2025-09-06 08:41:54 +0800

    refactor(rules): simplify Ad Click Guard to ignore ASN and block all non-mobile ad clicks

--- main.tf
+++ main.tf
@@ -27,27 +27,17 @@ resource "cloudflare_ruleset" "custom_waf" {
       enabled     = true
       expression  = <<-EOT
 not cf.client.bot
-and ip.geoip.asnum ne 15169
 and (
   lower(http.request.uri.query) contains "gclid="
   or lower(http.request.uri.query) contains "gbraid="
   or lower(http.request.uri.query) contains "wbraid="
   or lower(http.request.uri.query) contains "gad_source="
 )
-and (
-  ip.geoip.asnum in {
-    16509 14618 8075 31898 16276 24940 14061 20473 63949 12876
-    9009 51167 60781 16265 28753 30633 3223 40676 20454 54825
-    32181 40009 38136 199524 202425 45102 37963 45177 132203 45090
-    21859 31034 23470 35908 15003 8100 62904 14742 60068 49544
-    29802 18450 46475 26347 396982
-  }
-  or not (
-    lower(http.user_agent) contains "iphone"
-    or ( lower(http.user_agent) contains "android" and lower(http.user_agent) contains "mobile" )
-    or lower(http.user_agent) contains "windows phone"
-    or lower(http.user_agent) contains "ipod"
-  )
+and not (
+  lower(http.user_agent) contains "iphone"
+  or ( lower(http.user_agent) contains "android" and lower(http.user_agent) contains "mobile" )
+  or lower(http.user_agent) contains "windows phone"
+  or lower(http.user_agent) contains "ipod"
 )
 EOT
     },
